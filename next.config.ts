9fc46147ad59e8fe466c3c68afa0fc1d300ded7b
Jonathan <many.d.r.o.2001@hotmail.com>
2026-04-28 21:54:30 -0500

    Modified the way api is accessed

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,14 @@ const nextConfig: NextConfig = {
   turbopack: {
     root: __dirname,
   },
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://localhost:3000/api/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
